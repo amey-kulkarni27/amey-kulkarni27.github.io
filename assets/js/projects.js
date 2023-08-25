337d7642018a0829952b9ea7dfd54d5fa85efde0
amey-kulkarni27 <amey2704@gmail.com>
2023-08-25 22:41:06 +0200

    carousel done

---
+++ assets/js/projects.js
@@ -0,0 +1,58 @@
+const DEFAULTSTROKEWIDTH = 0.5;
+const DEFAULTIMAGESIZE = 30;
+
+class projectPage {
+  constructor(){
+    const carou = document.querySelector(".carousel"); // store carousel element
+    const arrows = document.querySelectorAll(".wrapper i"); // store arrow buttons element
+    const cardWidth = carou.querySelector(".card").offsetWidth;
+    const cardGap = parseFloat(window.getComputedStyle(carou).getPropertyValue('gap'));
+
+    let shouldDrag = false, initCursorPos, initScrollPos;
+
+    arrows.forEach(button => {
+      button.addEventListener("click", () => {
+        if(button.id === "left")
+          carou.scrollLeft -= cardWidth + cardGap;
+        else
+          carou.scrollLeft += cardWidth + cardGap;
+      })
+    });
+
+    const dragStart = (e) => {
+      shouldDrag = true;
+      carou.classList.add("dragging");
+      // Store original cursor and carousel scroll positions
+      initCursorPos = e.pageX;
+      initScrollPos = carou.scrollLeft;
+    }
+
+    const dragStop = () => {
+      shouldDrag = false;
+      carou.classList.remove("dragging");
+    }
+
+    const dragging = (e) => {
+      if(shouldDrag == false) return;
+      const shift = e.pageX - initCursorPos;
+      carou.scrollLeft = initScrollPos - shift;
+    }
+
+    carou.addEventListener("mousedown", dragStart); // when the mouse is clicked
+    carou.addEventListener("mousemove", dragging); // when the mouse is moved
+    document.addEventListener("mouseup", dragStop); // when the mouse is lifted
+  }
+}
+
+function whenDocumentLoaded(action) {
+  if (document.readyState === "loading") {
+    document.addEventListener("DOMContentLoaded", action);
+  } else {
+    // `DOMContentLoaded` already fired
+    action();
+  }
+}
+
+function initProject() {
+  map_object = new projectPage();
+}
